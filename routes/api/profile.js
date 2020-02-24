8225aa0ed3d9c2ff6475ab19efd9e6ce7cb9d30f
ravijavi <jain.ravi654@gmail.com>
2020-02-24 15:25:31 -0500

    Impemented deletion of profile and user, add and delete of both experience and education, fixed and secured github repo issue, now more secure and can fetch public info of GH user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 //routes that have anything to do with profiles, fetching, adding, etc.
 
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -136,6 +138,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+//@route Delete api/profile
+//@desc Delete profile, user, and posts
+//@access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //remove the profile
+    await Profile.findOneAndRemove({ user: req.user.id }); //no variable needed for deletion
+
+    //this will remove the user
+    await Profile.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User successfully deleted' }); //send along the profiles that we get
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 //@route Get api/profile/user/:user_id
 //@desc Get profile by user ID
 //@access Public
@@ -156,4 +175,208 @@ router.get('/user/:user_id', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+//@route PUT api/profile/experience
+//@desc Add profile experience
+//@access Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body; //destructuring and pulling all the above data from req.body
+
+    const newExp = {
+      //this will deal with the data that the user submits and assigns the info from req
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp); //unshift will ensure the most recent additions appear first
+
+      await profile.save();
+
+      res.json(profile); //will come back into play with the front end later on
+      //Even though it's en embedded array and not its own document in the database, it will still have its own unique id
+      //Can have this structure in one collection rather than having relational stuff and tables, one of the advantages of mongodb
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+//@route DELETE api/profile/experience/:exp_id
+//@desc Delete experience from profile
+//@access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }); //getting profile of the user
+
+    //Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1); //getting the index and splicing it out
+
+    await profile.save(); //save the new, appended data
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route PUT api/profile/education
+//@desc Add profile education
+//@access Private
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is required')
+        .not()
+        .isEmpty(),
+      check('degree', 'Degree is required')
+        .not()
+        .isEmpty(),
+      check('fieldofstudy', 'Field of Study is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body; //destructuring and pulling all the above data from req.body
+
+    const newEdu = {
+      //this will deal with the data that the user submits and assigns the info from req
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEdu); //unshift will ensure the most recent additions appear first
+
+      await profile.save();
+
+      res.json(profile); //will come back into play with the front end later on
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+//@route DELETE api/profile/education/:edu_id
+//@desc Delete education from profile
+//@access Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }); //getting profile of the user
+
+    //Get remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1); //getting the index and splicing it out
+
+    await profile.save(); //save the new, appended data
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route GET api/profile/github/:username
+//@desc Get user repos from Github
+//@access Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body)); //body is a regular string w/ escape quotes, so has to be wrapped in json function
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
