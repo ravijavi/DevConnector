3d0164bb88a14a356a4146a6168d4a93933904c5
ravijavi <jain.ravi654@gmail.com>
2020-02-25 17:00:19 -0500

    Set up React and Concurrently, setup my React Router, implemented state hook, successfully integrated React and Node.js with the landing page and the register/login page

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,112 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+//Using axios to access backend with the below, commented out code
+//import axios from 'axios';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData; //destructuring data
+
+  const onChange = async e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value }); //by using e.target.name, we can use this key to format every field
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passowrds do not match');
+    } else {
+      console.log('SUCCESS');
+
+      //I plan to implement this with a redux action, this is a successful attempt to register with React and Node.js working in tandem, result checked out in MongoDB Atlas with the creation of a new user
+      /*
+      const newUser = {
+        name,
+        email,
+        password
+      };
+
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        };
+
+        const body = JSON.stringify(newUser);
+
+        // first parameter is the route, second is the data, third is config, which has the header value/content-type
+        const res = await axios.post('/api/users', body, config); //need to do this b/c axios returns a promise
+        console.log(res.data);
+      } catch (err) {
+        console.error(err.response.data);
+      }
+      */
+    }
+  };
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            value={password2}
+            onChange={e => onChange(e)}
+            minLength='6'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <Link to='/login'>Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Register;
