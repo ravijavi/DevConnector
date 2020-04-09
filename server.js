dc4d63af84904fa6e330d4904cb587d252cab9bd
ravijavi <jain.ravi654@gmail.com>
2020-04-09 17:12:37 -0400

    fixed some small errors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -10,7 +11,7 @@ connectDB();
 //init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+//app.get('/', (req, res) => res.send('API Running')); //Must omit if you want to deploy on the server
 
 //make sure we can actually access the routes i.e. Define Routes
 app.use('/api/users', require('./routes/api/users')); //going to make api/users pertain to '/' in the users.js file
@@ -18,6 +19,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.port || 5000;
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
